005bca474228a58a11f5468b64a5a2e9f009a87d
Dmytro Hlybytskyi <dmytro.hlybytskyi@bini.games>
2023-04-02 00:57:47 +0300

    Improve artifacts panel, use joystick

--- ExisCris5/Assets/2D Platformer/Scripts/LaserProjectile.cs
+++ ExisCris5/Assets/2D Platformer/Scripts/LaserProjectile.cs
@@ -4,17 +4,21 @@ using UnityEngine;
 
 public class LaserProjectile : MonoBehaviour
 {
+    public float TotalDuration = 2f;
     public float DestructionDelay = 0.125f;
     public GameObject ExplosionTemplate;
-    
+
+    private float startTime;
+
     void Start()
     {
-        
+        startTime = Time.time;
     }
 
     void Update()
     {
-        
+        if (Time.time > startTime + TotalDuration)
+            Destroy(gameObject);
     }
 
     private void OnCollisionEnter2D(Collision2D other)
