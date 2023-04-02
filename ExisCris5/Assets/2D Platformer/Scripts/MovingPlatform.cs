86249bd7ecc356d058ec7a95ee24125bed47cb12
Dmytro Hlybytskyi <dmytro.hlybytskyi@bini.games>
2023-04-02 05:19:08 +0300

    Fix MovingPlatform

--- ExisCris5/Assets/2D Platformer/Scripts/MovingPlatform.cs
+++ ExisCris5/Assets/2D Platformer/Scripts/MovingPlatform.cs
@@ -2,15 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovingPlatfirm : MonoBehaviour
+public class MovingPlatform : MonoBehaviour
 {
     public float Speed = 1;
     public List<Transform> Waypoints;
 
     private float progress;
 
+    private Rigidbody2D rigidbody;
+    
     void Start()
     {
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
     void Update()
@@ -22,6 +25,8 @@ public class MovingPlatfirm : MonoBehaviour
         var pointB = Waypoints[1].position;
         progress = (Time.time * Speed) % 1f;
         progress = 1f - Mathf.Abs(progress * 2 - 1);
-        transform.position = Vector3.Lerp(pointA, pointB, progress);
+        var oldPosition = transform.position;
+        var newPosition = Vector3.Lerp(pointA, pointB, progress);
+        rigidbody.velocity = newPosition - oldPosition;
     }
 }
